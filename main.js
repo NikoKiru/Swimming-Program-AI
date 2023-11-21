5f7cbc34f39b826edc286967d08350dea2de0e6e
dknik <dknikolajk@gmail.com>
2023-11-21 15:22:38 +0100

    Addad Enter button

--- main.js
+++ main.js
@@ -5,12 +5,22 @@ const inputtedStroke = document.querySelector('input#stroke');
 const pTag = document.querySelector('p.program');
 const loadingP = document.querySelector('p.hidden');
 
-button.addEventListener("click", () => {
+function generateProgram() {
     loadingP.classList.remove('hidden')
     getGeneratedText(`Genarate a swimming trainning program. With a distance of: ${inputtedDistance.value} meters. Focus on: ${inputtedStroke.value} stroke.`)
         .then(generatedText => {
             pTag.innerText = generatedText
             loadingP.classList.add('hidden');
         });
+}
+
+button.addEventListener("click", generateProgram);
+
+[inputtedDistance, inputtedStroke].forEach(input => {
+    input.addEventListener('keydown', (event) => {
+        if (event.keyCode === 13) {  // 13 is the key code for Enter
+            generateProgram();
+        }
+    });
 });
 
